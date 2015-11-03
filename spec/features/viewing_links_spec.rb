deabbe7bbff9fe5db159d40f7d846a4305d47fa4
alaanzr <alaanzr@gmail.com>
2015-11-03 21:18:34 +0000

    End of challenge 09 - Practice

--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,10 +1,10 @@
 feature "Viewing links" do
   scenario "I can see existing links on the links page" do
-    visit('/')
+    Link.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
+    visit('/links')
     # fill_in('add_title', with: 'Makers')
     # fill_in('add_url', with: 'http://www.makersacademy.com')
     # click_button('add_link')
-    Link.create(title: 'Makers Academy', url: 'http://www.makersacademy.com')
     within('ul#links') do
       expect(page).to have_content('Makers Academy')
     end
