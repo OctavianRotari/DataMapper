9b764cf291295e4dca2ded60ac7245d98ebde662
alaanzr <alaanzr@gmail.com>
2015-11-04 11:57:00 +0000

    End of challenge 12

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,23 @@
 require 'capybara/rspec'
 require './app/app'
+require 'database_cleaner'
+
 Capybara.app = DataRecorder
 
 RSpec.configure do |config|
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -11,5 +26,6 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
 
 end
